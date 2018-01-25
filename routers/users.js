1bfa3318d885a5345b20c21efc25a47bef6ea3bf
GeneTinderholm <tinderholmgene@gmail.com>
2018-01-24 21:31:35 -0600

    user profiles up

--- routers/users.js
+++ routers/users.js
@@ -1,8 +1,28 @@
 var express = require('express');
 var router = express.Router();
+let mongoose = require('mongoose');
+let models = require('./../models');
+let Project = mongoose.model('Project');
+let User = mongoose.model('User');
+let Tasks = mongoose.model('Task');
 
-router.get('*', (req, res) => {
-  res.render('users/placeholder');
+router.get('/:id', (req, res, next) => {
+  User.findById(req.params.id).populate("projects").populate({path:"tasks"})
+    .then(user => {
+      // user.date = 
+      let userMod = {
+        id: user.id,
+        username: user.username,
+        date: user.date.toString().substring(4,15),
+        projects: user.projects,
+        tasks: user.tasks
+      }
+      res.render('users/single', {user: userMod});
+    })
+    .catch(error => {
+      next(error);
+    });
+
 })
 
 module.exports = router;
